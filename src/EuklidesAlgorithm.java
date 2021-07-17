8648f8143e80f12dc2d2683e93523c5e4f545ecf
ZanRafal <01159225@pw.edu.pl>
2021-07-17 23:37:06 +0200

    Added both versions of Euklid algorithm

---
+++ src/EuklidesAlgorithm.java
@@ -0,0 +1,17 @@
+public class EuklidesAlgorithm {
+
+    //recursive version of Euklid algorithm
+    public int calculateGCD(int a, int b) {
+        if(b == 0) return a;
+        else return calculateGCD(b, a % b);
+    }
+
+    //non-recursive version of Euklid algorithm
+    public int calculateGCD2(int a, int b) {
+        while (a != b) {
+            if(a > b) a -= b;
+            if(b > a) b -= a;
+        }
+        return a;
+    }
+}
